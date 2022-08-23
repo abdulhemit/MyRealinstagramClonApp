ba5436042d232918f556634afed9dd5945dd9cc9
ibrahim abdulhemit <93004625+abdulhemit@users.noreply.github.com>
2022-08-23 16:10:45 +0300

    gercek instagram uygulamasi ikkinci gonderi, bottomNavigatione fragmentler eklendi ve baslatildi

--- app/src/main/java/com/example/myrealinatagremclonapp/MainActivity.kt
+++ app/src/main/java/com/example/myrealinatagremclonapp/MainActivity.kt
@@ -3,50 +3,65 @@ package com.example.myrealinatagremclonapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.myrealinatagremclonapp.Fragments.HomeFragment
+import com.example.myrealinatagremclonapp.Fragments.NotificationFragment
+import com.example.myrealinatagremclonapp.Fragments.ProfileFragment
+import com.example.myrealinatagremclonapp.Fragments.SearchFragment
 import com.example.myrealinatagremclonapp.databinding.ActivityMainBinding
+import com.example.myrealinatagremclonapp.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    internal var selectedFragments: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            HomeFragment()
+        ).commit()
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                binding.message.setText("Home")
-                return@OnNavigationItemSelectedListener true
+
+                selectedFragments = HomeFragment()
             }
             R.id.nav_search -> {
-                binding.message.setText("search")
-                return@OnNavigationItemSelectedListener true
+
+                selectedFragments = SearchFragment()
             }
             R.id.nav_add_post -> {
-                binding.message.setText(" add post")
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                binding.message.setText("notifications")
-                return@OnNavigationItemSelectedListener true
+
+                selectedFragments = NotificationFragment()
             }
             R.id.nav_profile -> {
-                binding.message.setText("Profile")
-                return@OnNavigationItemSelectedListener true
+
+                selectedFragments = ProfileFragment()
             }
         }
-
+        if(selectedFragments != null){
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                selectedFragments!!
+            ).commit()
+        }
         false
     }
 }
